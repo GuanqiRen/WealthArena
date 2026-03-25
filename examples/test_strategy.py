a3834b0aeff4004139b3f2fe8e58ade88695a139
Guanqi Ren <renguanqi@gmail.com>
2026-03-24 21:27:39 -0400

    ignore frontend/node_modules

---
+++ examples/test_strategy.py
@@ -0,0 +1,23 @@
+from python_sdk import TradingClient, TradingClientError
+
+API_URL = "http://localhost:8000"
+USER_EMAIL = 'guanqi.ren@bofa.com'
+USER_PASSWORD = 'ready2go'
+
+client = TradingClient(
+    base_url=API_URL, 
+    email=USER_EMAIL, 
+    password=USER_PASSWORD,
+)
+
+client.register(USER_EMAIL, USER_PASSWORD)
+client.login()
+client.list_portfolios()
+
+client.create_portfolio('SPY_BENCHMARK')
+client.get_portfolio('SPY_BENCHMARK')
+
+portfolios = client.list_portfolios()
+client.place_order(portfolios[0].id, 'SPY', 10, 'buy')
+
+client.create_portfolio('Strategy_1')
